927d160e2af9d9a0c926c48f0ddc42899747e9bf
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-01-20 20:57:53 -0300

    Added: Title component

---
+++ src/components/Title/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from "@chakra-ui/react";
+
+export function Title() {
+    return <>
+        <Text fontSize="2xl" fontWeight="bold">
+            Title
+        </Text>
+    </>;
+}
