29661abf0e956bb1e49bb76140df500beef13a77
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-01-25 23:52:15 -0300

    Added: data structure definition

---
+++ src/data/listOfVerbs.js
@@ -0,0 +1,22 @@
+export const listOfVerbs = [
+    {
+        "id": 1,
+        "type": "I",
+        "simpleForm": "Abide",
+        "thirdPerson": "Abides",
+        "simplePast": "Abode",
+        "pastParticiple": "Abode",
+        "gerund": "Abiding",
+        "meaning": "Morar - Habitar - Tolerar",
+    },
+    {
+        "id": 1,
+        "type": "R",
+        "simpleForm": "Absorb",
+        "thirdPerson": "Absorbs",
+        "simplePast": "Absorbed",
+        "pastParticiple": "Absorbed",
+        "gerund": "Absorbing",
+        "meaning": "Absorber",
+    },
+];
