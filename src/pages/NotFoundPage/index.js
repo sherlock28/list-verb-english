772355a6a0e46e6063890c2bf8b322b7f0534143
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-01-31 07:29:09 -0300

    Added: NotFound page

---
+++ src/pages/NotFoundPage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+
+export function NotFoundPage() {
+  return (
+    <>
+      <Box>
+        <Heading as="h2" size="xl">404, Not Found!</Heading>
+      </Box>
+    </>
+  );
+}
