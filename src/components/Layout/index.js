f0cceb55e8f553d01878462168972801659d19e9
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-02-04 06:51:46 -0300

    Changed: Increased top margin for Layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from '@chakra-ui/react'
 
 export function Layout({ children }) {
-    return <Container maxW='6xl' centerContent>
+    return <Container maxW='6xl' mt={10} centerContent>
         {children}
     </Container>;
 }
