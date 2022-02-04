99c16a9f1b9ff26d88bc10f1266d755cc4e26efe
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-02-04 06:53:17 -0300

    Changed: Search component defined

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import { Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Input, Box, Text } from "@chakra-ui/react";
 
 export function Search() {
-    return <>
-        <Text fontSize='2xl'>Search</Text>
-    </>;
+    const [query, setQuery] = useState("");
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        console.log("search")
+    }
+
+    const handleChange = e => {
+        setQuery(e.target.value);
+        // search();
+    }
+
+    return (
+        <form onSubmit={handleSearch}>
+            <Box d="flex" flexDir="column" justifyContent="center">
+                <Input
+                    name="queryField"
+                    onChange={handleChange}
+                    variant="flushed"
+                    type="search"
+                    placeholder="Verb..."
+                    size="lg"
+                    w={["100%", "340px", "440px", "500px"]}
+                    my={8}
+                />
+            </Box>
+        </form>
+    );
 }
