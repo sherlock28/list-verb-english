3d6f7b4123492139d68aa12c32685a29cf71d4a4
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-05-18 06:22:16 -0300

    Added: The useSearchVerbs hook that implements the functionality to search for verbs

---
+++ src/components/Search/hooks/useSearchVerbs.js
@@ -0,0 +1,34 @@
+import { useState, useEffect, useContext } from "react";
+import { listOfVerbs } from "data/listOfVerbs";
+import VerbsContext from "context/VerbsContext";
+
+export function useSearchVerbs() {
+    const { filteredVerbs, setFilteredVerbs } = useContext(VerbsContext);
+
+    const [message, setMessage] = useState("");
+    const [target, setTarger] = useState("");
+
+    const handleChange = e => {
+        setTarger(e.target.value);
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // filter();
+    }
+    
+    useEffect(() => filter(), [target]);
+
+    const filter = () => {
+        setFilteredVerbs(listOfVerbs.filter(verb => verb.simpleForm.toLowerCase().indexOf(target.toLowerCase()) !== -1));
+
+        if (filteredVerbs.length === 0) {
+            setMessage("Verbo no encontrado");
+        }
+        return;
+    }
+
+    return {
+        handleChange, handleSubmit, filteredVerbs, message
+    }
+}
